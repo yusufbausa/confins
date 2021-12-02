a7863743d244902c4e57b9532e69a59cc9e6406a
Yusuf Bausa <yusuf.bausa@gmail.com>
2021-12-02 14:22:55 +0700

    November optimization

--- Test Suites/Customer/TS - Add New - Personal Customer - v2.ts
+++ Test Suites/Customer/TS - Add New - Personal Customer - v2.ts
@@ -20,8 +20,8 @@
          <combinationType>ONE</combinationType>
          <id>9a318366-6cca-468f-89e2-a4cd074c727a</id>
          <iterationEntity>
-            <iterationType>RANGE</iterationType>
-            <value>2-5</value>
+            <iterationType>SPECIFIC</iterationType>
+            <value>8</value>
          </iterationEntity>
          <testDataId>Data Files/Customer/Personal Customer/Data New Customer</testDataId>
       </testDataLink>
